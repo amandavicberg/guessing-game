b0a056306df89c2f14a49e70df2ee8aabc2b55a5
amandabergamasco <amandavicbergamasco@gmail.com>
2026-06-21 16:12:43 -0300

    finalizado jogo de adivinhacao

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use std::io;
 use rand::{Rng, RngExt};
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Adivinhe o número :)");
-
     let secret_number = rand::rng().random_range(1..=100);
 
-    println!("O número aleatório é: {}", secret_number);
+    loop {
+        println!("*Jogo de adivinhar o número*");
+        println!();
+        println!("Digite o seu palpite:");
 
-    println!("Digite o seu palpite:");
+        let mut guess= String::new();
 
-    let mut guess= String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler entrada");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Falha ao ler entrada");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Você disse: {}", guess);
+        println!();
+        println!("Você disse: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Seu palpite foi muito baixo"),
+            Ordering::Greater => println!("Seu palpite foi muito alto"),
+            Ordering::Equal => {
+                println!("Parabéns. Você acertou!");
+                break;
+            }
+        };
+        println!();
+    }
 }
